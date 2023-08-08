ff1be35a28bdf5cd26cce0f11238d7efd863b22d
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-08-08 18:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    // Get the input value
+    const radius = parseFloat(document.getElementById('radius').value);
+    
+    // Calculate the volume
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    
+    // Display the volume
+    document.getElementById('volume').value = volume;
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    // Prevent the form from submitting and refreshing the page
+    return false;
+}
+
+// Set the volume_sphere function to be called when the form is submitted
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
